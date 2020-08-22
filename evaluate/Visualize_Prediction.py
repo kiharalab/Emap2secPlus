c6aa2a4a0f961c0a5e569eb962b6b65774e28fad
wang3702 <wang3702@purdue.edu>
2020-08-22 15:13:08 -0400

    update on license claim

--- evaluate/Visualize_Prediction.py
+++ evaluate/Visualize_Prediction.py
@@ -1,3 +1,37 @@
+# Publication:  "Emap2sec+:  Detecting Protein and DNA/RNA Structures in Cryo-EM Maps of Intermediate Resolution Using Deep Learning", Xiao Wang, Eman Alnabati, Tunde W. Aderinwale, Sai Raghavendra Maddhuri Venkata Subramaniya, Genki Terashi, and Daisuke Kihara, BioRxiv (2020)
+
+# Emap2sec+ is a computational tool using deep learning that can accurately identify structures, alpha helices, beta sheets, other(coils/turns) and DNA/RNA, in cryo-Electron Microscopy (EM) maps of medium to low resolution.
+# Copyright (C) 2020 Xiao Wang, Eman Alnabati, Tunde W Aderinwale, Sai Raghavendra Maddhuri, Genki Terashi, Daisuke Kihara, and Purdue University.
+# License: GPL v3 for academic use. (For commercial use, please contact us for different licensing.)
+# Contact: Daisuke Kihara (dkihara@purdue.edu)
+
+
+#
+
+# This program is free software: you can redistribute it and/or modify
+
+# it under the terms of the GNU General Public License as published by
+
+# the Free Software Foundation, either version 3 of the License, or
+
+# (at your option) any later version.
+
+#
+
+# This program is distributed in the hope that it will be useful,
+
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+# GNU General Public License V3 for more details.
+
+#
+
+# You should have received a copy of the GNU v3.0 General Public License
+
+# along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+
 import os
 import numpy as np
 from ops.os_operation import mkdir
--- process_map/cp.h
+++ process_map/cp.h
@@ -1,3 +1,34 @@
+/*
+# Emap2sec+ is a computational tool using deep learning that can accurately identify structures, alpha helices, beta sheets, other(coils/turns) and DNA/RNA, in cryo-Electron Microscopy (EM) maps of medium to low resolution.
+# Copyright (C) 2020 Xiao Wang, Eman Alnabati, Tunde W Aderinwale, Sai Raghavendra Maddhuri, Genki Terashi, Daisuke Kihara, and Purdue University.
+# License: GPL v3 for academic use. (For commercial use, please contact us for different licensing.)
+# Contact: Daisuke Kihara (dkihara@purdue.edu)
+
+
+#
+
+# This program is free software: you can redistribute it and/or modify
+
+# it under the terms of the GNU General Public License as published by
+
+# the Free Software Foundation, version 3.
+
+#
+
+# This program is distributed in the hope that it will be useful,
+
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+# GNU General Public License V3 for more details.
+
+#
+
+# You should have received a copy of the GNU v3.0 General Public License
+
+# along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+*/
 #include <cmath>
 #include <vector>
 #include <string>
